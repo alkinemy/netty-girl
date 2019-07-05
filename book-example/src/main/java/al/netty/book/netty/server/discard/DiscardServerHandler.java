e158a6310601d73a16c6b172095531c7792d8c08
joke111 <alta.second@gmail.com>
2019-07-05 15:28:43 +0900

    merge old netty book codes

---
+++ book-example/src/main/java/al/netty/book/netty/server/discard/DiscardServerHandler.java
@@ -0,0 +1,17 @@
+package al.netty.book.netty.server.discard;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+public class DiscardServerHandler extends SimpleChannelInboundHandler<Object> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //do nothing
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
